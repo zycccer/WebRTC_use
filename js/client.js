9c0c945707f5e20bca324e1923e8952b0f03589d
怪叔叔 <191462660@qq.com>
2022-03-18 17:36:50 +0800

    本地基础端到端的连接

--- js/client.js
+++ js/client.js
@@ -11,16 +11,23 @@ var filtersSelect = document.querySelector('select#filter');
 //picture
 var snapshot = document.querySelector('button#snapshot');
 var picture = document.querySelector('canvas#picture');
-
-var divConstraints = document.querySelector('div#constraints');
-
+picture.width = 640;
+picture.height = 480;
 
 var videoplay = document.querySelector('video#player');
-var audioplay = document.querySelector('audio#audioplayer');
 
 
+//div
+var divConstraints = document.querySelector('div#constraints');
 
+//record
+var recvideo = document.querySelector('video#recplayer');
+var btnRecord = document.querySelector('button#record');
+var btnPlay = document.querySelector('button#recplay');
+var btnDownload = document.querySelector('button#download');
 
+var buffer;
+var mediaRecorder;
 
 
 
@@ -44,16 +51,16 @@ function gotDevices(deviceInfos){
 
 function gotMediaStream(stream){
 
-
-    videoplay.srcObject = stream;
-
-    // audioplay.srcObject = stream;
     var videoTrack = stream.getVideoTracks()[0];
     var videoConstraints = videoTrack.getSettings();
 
     divConstraints.textContent = JSON.stringify(videoConstraints, null, 2);
-    return navigator.mediaDevices.enumerateDevices();
 
+    window.stream = stream;
+    videoplay.srcObject = stream;
+
+    //audioplay.srcObject = stream;
+    return navigator.mediaDevices.enumerateDevices();
 }
 
 function handleError(err){
@@ -81,9 +88,8 @@ function start() {
             },
             audio : false
         }
-
-        // navigator.mediaDevices.getDisplayMedia(constraints)
-        navigator.mediaDevices.getUserMedia(constraints)
+        navigator.mediaDevices.getDisplayMedia(constraints)
+        // navigator.mediaDevices.getUserMedia(constraints)
             .then(gotMediaStream)
             .then(gotDevices)
             .catch(handleError);
@@ -96,12 +102,82 @@ videoSource.onchange = start;
 
 filtersSelect.onchange = function(){
     videoplay.className = filtersSelect.value;
-
 }
 
 snapshot.onclick = function() {
     picture.className = filtersSelect.value;
-    picture.getContext('2d').drawImage(videoplay, 0, 0, 480, 640);
+    picture.getContext('2d').drawImage(videoplay, 0, 0, picture.width, picture.height);
+}
+
+function handleDataAvailable(e){
+    if(e && e.data && e.data.size > 0){
+        buffer.push(e.data);
+    }
+}
+
+function startRecord(){
+
+    buffer = [];
+
+    var options = {
+        mimeType: 'video/webm;codecs=vp8'
+    }
+
+    if(!MediaRecorder.isTypeSupported(options.mimeType)){
+        console.error(`${options.mimeType} is not supported!`);
+        return;
+    }
+
+    try{
+        mediaRecorder = new MediaRecorder(window.stream, options);
+    }catch(e){
+        console.error('Failed to create MediaRecorder:', e);
+        return;
+    }
+
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(10);
+
+}
+
+function stopRecord(){
+    mediaRecorder.stop();
+}
+
+btnRecord.onclick = ()=>{
+
+    if(btnRecord.textContent === 'Start Record'){
+        startRecord();
+        btnRecord.textContent = 'Stop Record';
+        btnPlay.disabled = true;
+        btnDownload.disabled = true;
+    }else{
+
+        stopRecord();
+        btnRecord.textContent = 'Start Record';
+        btnPlay.disabled = false;
+        btnDownload.disabled = false;
+
+    }
+}
+
+btnPlay.onclick = ()=> {
+    var blob = new Blob(buffer, {type: 'video/webm'});
+    recvideo.src = window.URL.createObjectURL(blob);
+    recvideo.srcObject = null;
+    recvideo.controls = true;
+    recvideo.play();
+}
+
+btnDownload.onclick = ()=> {
+    var blob = new Blob(buffer, {type: 'video/webm'});
+    var url = window.URL.createObjectURL(blob);
+    var a = document.createElement('a');
+
+    a.href = url;
+    a.style.display = 'none';
+    a.download = 'aaa.webm';
+    a.click();
 }
 
 
